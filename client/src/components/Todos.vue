0e7b5e09698d9c0b28882ff2cb689ff8fdbe5093
tkatska <olgat@iqmetrix.com>
2020-02-02 09:23:11 -0800

    fixing indentation

--- client/src/components/Todos.vue
+++ client/src/components/Todos.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="container">
-    <div class="col-sm-10">
+  <div class='container'>
+    <div class='col-sm-10'>
       <h1>Задачи</h1>
-      <confirmation :message="confirmationMessage" v-if="showConfirmation"></confirmation>
+      <confirmation :message='confirmationMessage' v-if='showConfirmation'></confirmation>
 
       <button
-        type="button"
-        id="task-add"
-        class="btn btn-success btn-sm align-left d-block"
+        type='button'
+        id='task-add'
+        class='btn btn-success btn-sm align-left d-block'
         v-b-modal.todo-modal
       >Добавить задачу</button>
 
-      <table class="table table-dark table-stripped table-hover">
-        <thead class="thead-light">
+      <table class='table table-dark table-stripped table-hover'>
+        <thead class='thead-light'>
           <tr>
             <th>Uid</th>
             <th>Описание</th>
@@ -22,55 +22,31 @@
         </thead>
 
         <tbody>
-          <tr v-for="(todo, index) in todos" :key="index">
-            <td class="todo-uid">{{ todo.uid }}</td>
+          <tr v-for='(todo, index) in todos' :key='index'>
+            <td class='todo-uid'>{{ todo.uid }}</td>
             <td>{{ todo.description }}</td>
             <td>
-              <span v-if="todo.is_completed">Выполнено</span>
+              <span v-if='todo.is_completed'>Выполнено</span>
               <span v-else>Не выполнено</span>
             </td>
             <td>
-              <div class="btn-group" role="group">
+              <div class='btn-group' role='group'>
                 <!-- <button type='button' class='btn btn-secondary btn-sm'>Обновить</button> -->
                 <button
-                  type="button"
-                  class="btn btn-secondary btn-sm"
+                  type='button'
+                  class='btn btn-secondary btn-sm'
                   v-b-modal.todo-update-modal
-                  @click="updateTodo(todo)"
+                  @click='updateTodo(todo)'
                 >Обновить</button>
                 &nbsp;
                 <!-- <button type='button' class='btn btn-danger btn-sm'>X</button> -->
-                <button type="button" class="btn btn-danger btn-sm" @click="deleteTodo(todo)">X</button>
+                <button type='button' class='btn btn-danger btn-sm'
+                @click='deleteTodo(todo)'>X</button>
               </div>
             </td>
           </tr>
         </tbody>
       </table>
-
-      <b-modal ref="addTodoModal" id="todo-modal" title="Добавить задачу" hide-footer>
-        <b-form @submit="onSubmit" @reset="onReset" class="w-100">
-          <b-form-group
-            id="form-description-group"
-            label="Описание:"
-            label-for="form-description-input"
-          >
-            <b-form-input
-              id="form-description-input"
-              type="text"
-              v-model="addTodoForm.description"
-              required
-              placeholder="Завести задачу"
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group id="form-complete-group">
-            <b-form-checkbox-group v-model="addTodoForm.is_completed" id="form-checks">
-              <b-form-checkbox value="true">Задача выполнена?</b-form-checkbox>
-            </b-form-checkbox-group>
-          </b-form-group>
-          <b-button type="submit" variant="primary">Добавить</b-button>
-          <b-button type="reset" variant="danger">Сброс</b-button>
-        </b-form>
-      </b-modal>
 
       <b-modal ref='addTodoModal' id='todo-modal' title='Добавить задачу' hide-footer>
         <b-form @submit='onSubmit' @reset='onReset' class='w-100'>
@@ -141,39 +117,39 @@ td {
 </style>
 
 <script>
-import axios from "axios";
-import Comformation from "./Comformation.vue";
+import axios from 'axios';
+import Comformation from './Comformation.vue';
 
-const dataURL = "http://localhost:5000/api/tasks/";
+const dataURL = 'http://localhost:5000/api/tasks/';
 
 export default {
-  name: "Todo",
+  name: 'Todo',
   data() {
     return {
       todos: [],
       addTodoForm: {
-        description: "",
-        is_completed: []
+        description: '',
+        is_completed: [],
       },
       updateTodoForm: {
         uid: 0,
-        description: "",
-        is_completed: []
+        description: '',
+        is_completed: [],
       },
-      confirmationMessage: "",
-      showConfirmation: false
+      confirmationMessage: '',
+      showConfirmation: false,
     };
   },
   methods: {
     getTodos() {
-      axios.get(dataURL).then(response => {
+      axios.get(dataURL).then((response) => {
         this.todos = response.data.tasks;
       });
     },
     resetForm() {
-      this.addTodoForm.description = "";
+      this.addTodoForm.description = '';
       this.addTodoForm.is_completed = [];
-      this.updateTodoForm.description = "";
+      this.updateTodoForm.description = '';
       this.updateTodoForm.is_completed = [];
     },
     onSubmit(event) {
@@ -181,11 +157,11 @@ export default {
       this.$refs.addTodoModal.hide();
       const requestData = {
         description: this.addTodoForm.description,
-        is_completed: this.addTodoForm.is_completed[0]
+        is_completed: this.addTodoForm.is_completed[0],
       };
       axios.post(dataURL, requestData).then(() => {
         this.getTodos();
-        this.confirmationMessage = `Задача "${requestData.description}" добавлена`;
+        this.confirmationMessage = `Задача '${requestData.description}' добавлена`;
         this.showConfirmation = true;
       });
       this.resetForm();
@@ -196,41 +172,42 @@ export default {
       this.resetForm();
     },
     updateTodo(todo) {
-      this.updateTodo = todo;
+      this.updateTodoForm = todo;
     },
     onUpdateSubmit(event) {
       event.preventDefault();
       this.$refs.updateTodoModal.hide();
       const requestData = {
         description: this.updateTodoForm.description,
-        is_completed: this.updateTodoForm.is_completed[0]
+        is_completed: this.updateTodoForm.is_completed[0],
       };
       const todoURL = dataURL + this.updateTodoForm.uid;
-      axios.put(todoURL, requestData).then(() => {
-        this.getTodos();
-        this.confirmationMessage = "Задача обновлена";
-        this.showConfirmation = true;
-      });
+      axios.put(todoURL, requestData)
+        .then(() => {
+          this.getTodos();
+          this.confirmationMessage = 'Задача обновлена';
+          this.showConfirmation = true;
+        });
     },
     onUpdateReset(event) {
       event.preventDefault();
       this.$refs.updateTodoModal.hide();
       this.resetForm();
     },
-    deleteTodo(todo) {
-      const todoURL = dataURL + todo.uid;
-      axios.delete(todoURL).then(() => {
-        this.getTodos();
-        this.confirmationMessage = "Задача удалена из списка";
-        this.showConfirmation = true;
-      });
-    }
+  },
+  deleteTodo(todo) {
+    const todoURL = dataURL + todo.uid;
+    axios.delete(todoURL).then(() => {
+      this.getTodos();
+      this.confirmationMessage = 'Задача удалена из списка';
+      this.showConfirmation = true;
+    });
   },
   components: {
-    confirmation: Comformation
+    confirmation: Comformation,
   },
   created() {
     this.getTodos();
-  }
+  },
 };
 </script>
