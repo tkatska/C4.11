d5e85b8c7d91b23fbcce79d0ea4dc165642cd44d
tkatska <olgat@iqmetrix.com>
2020-02-01 22:55:04 -0800

    adding projects

---
+++ client/src/components/Comformation.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <b-alert variant="success" show dismissible>{{ message }}</b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message'],
+};
+</script>
---
+++ client/src/components/Fetch.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1>Todos</h1>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      Бутстрап подключен и работает
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  </div>
+</template>
+
